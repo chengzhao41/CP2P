eeebcefea057601db6ceef06d93779c081cda879
Ladislav Rampasek <rampasek@gmail.com>
2015-12-16 19:44:55 -0500

    data processing separated from plotting + SVA preprocessing of cell lines

--- SVA_plots/data_processing_plots.R
+++ SVA_plots/data_processing_plots.R
@@ -5,9 +5,10 @@ source("Common/preparing_data_helper.R")
 source("Common/comGENE.R")
 
 load("Bortezomib/WS/bortezomib_data.RData"); bortezomib$patient <- bortezomib$patient.combat
-load("Docetaxel/WS/docetaxel_data.RData")
+# load("Docetaxel/WS/docetaxel_data.RData")
 # load("Erlotinib/WS/erlotinib_data.RData")
 # load("Epirubicin/WS/epirubicin_data.RData")
+
 
 generate_single_random_partition.cpp_var <- function(input_labels_cell_lines, input_labels_patient, training_amount) {
   ## Copied and adjusted from Bortezomib/Script/generate_random_partition.R function generate_random_partition.cpp_var
@@ -50,25 +51,8 @@ generate_single_random_partition.cpp_var <- function(input_labels_cell_lines, in
   return(cpp.partition)
 }
 
-format_PCAplot <- function (plt, plot_label, pOrder) {
-  plot_label <<- plot_label
-  plt <- plt + theme(legend.direction = 'horizontal', 
-                     legend.position = 'top', plot.margin = unit(c(5.1,7,4.5,3.5)/2, "lines"), 
-                     text = element_text(size=15), axis.title.x=element_text(vjust=-1.5)) + 
-    scale_color_discrete(name = 'Dataset')
-  plt <- plt + guides(colour = guide_legend(override.aes = list(size=2.5, linetype=0, shape = c(16, 17, 15))))
-  plt <- plt + geom_point(aes(shape = factor(plot_label), colour = factor(plot_label)), show_guide = FALSE)
-  plt <- plt + ggtitle(LETTERS[pOrder]) + theme(plot.title=element_text(hjust=-0.12, size = rel(1.75), face='bold'))
-  return(eval(ggplotGrob(plt)))
-}
 
-plot_processed_data_all <- function(data, label, name="drugX", n.sv = 0){
-  library(ggplot2)
-  library(grid)
-  library(RColorBrewer)
-  require(gtable)
-  require(gridExtra)
-  
+compute_preprocessing <- function(data, label, sampleinfo, n.sv = 0, preprocessCLsva = FALSE){
   ## Split to training set and patient-only test set
   labels_cell_lines <- label$AUC
   cl.len <- length(label$AUC)
@@ -78,34 +62,56 @@ plot_processed_data_all <- function(data, label, name="drugX", n.sv = 0){
   #print(length(cpp.partition$test_index))
   #print(length(cpp.partition$training_index))
   
-  ## Pool AUC, IC50, Slope data and labels together
-  #AUC
-  temp.data <- comGENE(scale(data$cgp_AUC), scale(data$patient))
-  data.raw <- list(rbind(temp.data[[1]], temp.data[[2]]))
-  labels.combined <- list(c(label$AUC, label$patient))
+  data.raw <- list()
+  labels.combined <- list()
+  ## Pool AUC, IC50, Slope data and labels together, preprocess cell lines with SVA first
+  if (preprocessCLsva) {
+    cgp_AUC.sva <- sva_combine(batch = sampleinfo$tissue.type[label$AUC_ind],  
+                                 label = label$AUC, input_data = scale(data$cgp_AUC), n.sv=3)
+    temp.data <- comGENE(scale(cgp_AUC.sva), scale(data$patient))
+  } else {
+    temp.data <- comGENE(scale(data$cgp_AUC), scale(data$patient))
+  }
+  data.raw[[1]] <- rbind(temp.data[[1]], temp.data[[2]])
+  labels.combined[[1]] <- c(label$AUC, label$patient)
   #IC50
-  temp.data <- comGENE(scale(data$cgp_IC50), scale(data$patient))
+  if (preprocessCLsva) {
+    cgp_IC50.sva <- sva_combine(batch = sampleinfo$tissue.type[label$IC50_ind],  
+                                label = label$IC50, input_data = scale(data$cgp_IC50), n.sv=3)
+    temp.data <- comGENE(scale(cgp_IC50.sva), scale(data$patient))
+  } else {
+    temp.data <- comGENE(scale(data$cgp_IC50), scale(data$patient))
+  }
   data.raw[[2]] <- rbind(temp.data[[1]], temp.data[[2]])
   labels.combined[[2]] <- c(label$IC50, label$patient)
   #Slope
-  temp.data <- comGENE(scale(data$cgp_slope), scale(data$patient))
+  if (preprocessCLsva) {
+    cgp_slope.sva <- sva_combine(batch = sampleinfo$tissue.type[label$slope_ind],  
+                                 label = label$slope, input_data = scale(data$cgp_slope), n.sv=3)
+    temp.data <- comGENE(scale(cgp_slope.sva), scale(data$patient))
+  } else {
+    temp.data <- comGENE(scale(data$cgp_slope), scale(data$patient))
+  }
   data.raw[[3]] <- rbind(temp.data[[1]], temp.data[[2]])
   labels.combined[[3]] <- c(label$slope, label$patient)
   
-  
-  ## Get AUC data and labels
-  temp.data.raw <- data.raw[[1]]
-  temp.labels.combined <- labels.combined[[1]]
-  
-  ## Cell lines, train patient and test patient labels for PCA plot
-  temp.plot_label <- cpp.partition$cpp_flags
-  
   pID <- 1
-  ## Plot PCA of the data as is
-  p1 <- show_pca(input_data = temp.data.raw, label = temp.plot_label, pca_line_plot = FALSE, print = FALSE)
-  g1 <- format_PCAplot(p1, temp.plot_label, pID)
+  data.all <- list()
+  plabel.all <- list()
+  ## Store raw data as is
+  # temp.data <- comGENE(scale(data$cgp_AUC), scale(data$patient))
+  temp.data <- comGENE(scale(data$cgp_slope), scale(data$patient))
+  temp.data.raw <- rbind(temp.data[[1]], temp.data[[2]])
+  temp.label.combined <- c(label$slope, label$patient)
+  
+  ## Compensate for different number of cell lines with AUC/IC50/Slope labels
+  temp.ddiff <- length(labels.combined[[1]]) - length(temp.label.combined)
+  print(paste("adjusting", temp.ddiff))
+  temp.plot_label <- cpp.partition$cpp_flags[(temp.ddiff+1) : length(cpp.partition$cpp_flags)]
+  
+  data.all[[pID]] <- temp.data.raw
+  plabel.all[[pID]] <- temp.plot_label
   pID <- pID + 1
-  lplots <- list(g1) # list to keep all plots in
   
   for(outcomeID in 1:3) {
     ## Get data and labels
@@ -125,40 +131,66 @@ plot_processed_data_all <- function(data, label, name="drugX", n.sv = 0){
     temp.test_index <- cpp.partition$test_index - temp.ddiff
     # print(paste("len3", length(temp.train_index), length(temp.test_index)))
     
-    ## Plot PCA of Combat processed data
+    ## Store Combat processed data
     temp.data.ComBat <- ComBat_combine(batch = temp.cpp_source,
-                                      label = temp.labels.combined,
-                                      input_data = temp.data.raw)
-    
-    p2 <- show_pca(input_data = temp.data.ComBat, label = temp.plot_label, pca_line_plot = FALSE, print = FALSE)
-    g2 <- format_PCAplot(p2, temp.plot_label, pID)
+                                       label = temp.labels.combined,
+                                       input_data = temp.data.raw)
+    data.all[[pID]] <- temp.data.ComBat
+    plabel.all[[pID]] <- temp.plot_label
     pID <- pID + 1
-    lplots <- c(lplots, list(g2))
     
-    ## Plot PCA of SVA processed data
+    ## Store SVA processed data
     temp.data.sva <- sva_combine(batch = temp.cpp_source,
                                  label = temp.labels.combined,
                                  input_data = temp.data.raw,
                                  n.sv = n.sv)
-    
-    p3 <- show_pca(input_data = temp.data.sva, label = temp.plot_label, pca_line_plot = FALSE, print = FALSE)
-    g3 <- format_PCAplot(p3, temp.plot_label, pID)
+    data.all[[pID]] <- temp.data.sva
+    plabel.all[[pID]] <- temp.plot_label
     pID <- pID + 1
-    lplots <- c(lplots, list(g3))
     
-    ## Plot PCA of frozen SVA processed data
+    ## Store frozen SVA processed data
     temp.data.fsva <- fsva_combine(batch = temp.cpp_source,
-                                 label = temp.labels.combined,
-                                 input_data = temp.data.raw,
-                                 training_ind = temp.train_index,
-                                 test_ind = temp.test_index,
-                                 n.sv = n.sv)
-                                 #num_sv_method = "leek"
-    
-    p4 <- show_pca(input_data = temp.data.fsva, label = temp.plot_label, pca_line_plot = FALSE, print = FALSE)
-    g4 <- format_PCAplot(p4, temp.plot_label, pID)
+                                   label = temp.labels.combined,
+                                   input_data = temp.data.raw,
+                                   training_ind = temp.train_index,
+                                   test_ind = temp.test_index,
+                                   n.sv = n.sv)
+    #num_sv_method = "leek"
+    data.all[[pID]] <- temp.data.fsva
+    plabel.all[[pID]] <- temp.plot_label
     pID <- pID + 1
-    lplots <- c(lplots, list(g4))
+  }
+  return(list(data.all, plabel.all))
+}
+
+
+format_PCAplot <- function (plt, plot_label, pOrder) {
+  plot_label <<- plot_label
+  plt <- plt + theme(legend.direction = 'horizontal', 
+                     legend.position = 'top', plot.margin = unit(c(5.1,7,4.5,3.5)/2, "lines"), 
+                     text = element_text(size=15), axis.title.x=element_text(vjust=-1.5)) + 
+    scale_color_discrete(name = 'Dataset')
+  plt <- plt + guides(colour = guide_legend(override.aes = list(size=2.5, linetype=0, shape = c(16, 17, 15))))
+  plt <- plt + geom_point(aes(shape = factor(plot_label), colour = factor(plot_label)), show_guide = FALSE)
+  plt <- plt + ggtitle(LETTERS[pOrder]) + theme(plot.title=element_text(hjust=-0.12, size = rel(1.75), face='bold'))
+  return(eval(ggplotGrob(plt)))
+}
+
+#### Plot PCA of all given data
+plot_processed_data_all <- function(data.all, plabel.all, name="drugX"){
+  library(ggplot2)
+  library(grid)
+  library(RColorBrewer)
+  require(gtable)
+  require(gridExtra)
+  
+  lplots <- list() # list to keep all plots in
+  
+  for(pID in 1:length(data.all)) {
+    ## Plot PCA of the data as is
+    p1 <- show_pca(input_data = data.all[[pID]], label = plabel.all[[pID]], pca_line_plot = FALSE, print = FALSE)
+    g1 <- format_PCAplot(p1, plabel.all[[pID]], pID)
+    lplots <- c(lplots, list(g1))
   }
   
 #   ## Add figure letters
@@ -175,14 +207,18 @@ plot_processed_data_all <- function(data, label, name="drugX", n.sv = 0){
   
   print("plotting")
   
-  pdf(paste(name, "_all_sva2.pdf", sep=""), width = 20, height = 25)#, onefile=FALSE)
+  pdf(paste(name, "_all_t4.pdf", sep=""), width = 20, height = 25)#, onefile=FALSE)
   #grid.arrange(blank, g1, blank, g2, g3, g4, nrow=2, ncol=3)
   grid.arrange(grobs=gs, nrow=4, ncol=3)
   dev.off()
 
-  }
+}
 
-plot_processed_data_all(bortezomib, bortezomib.labels, name="bortezomib", n.sv = 2)
-plot_processed_data_all(docetaxel, docetaxel.labels, name="docetaxel", n.sv = 2)
+# !!!TODO!!!!: check ComBat function and decide of preprocess CL data with SVA beforehand
+
+#processed.data <- compute_preprocessing(bortezomib, bortezomib.labels, sampleinfo.cgp, n.sv = 2, preprocessCLsva = TRUE)
+#plot_processed_data_all(processed.data[[1]], processed.data[[2]], name="bortezomib")
+
+# plot_processed_data_all(docetaxel, docetaxel.labels, name="docetaxel", n.sv = 2)
 # plot_processed_data_all(erlotinib, erlotinib.labels, name="erlotinib", n.sv = 2)
 # plot_processed_data_all(epirubicin, epirubicin.labels, name="epirubicin", n.sv = 2)
