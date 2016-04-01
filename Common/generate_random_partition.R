c10da2b964f6c21aae2d88221a56a8415e675c0c
Cheng Zhao <chengzhao41@gmail.com>
2016-03-31 23:33:34 -0700

    allow forcing of a balanced training and test set with at least 8 or 5 samples

--- Common/generate_random_partition.R
+++ Common/generate_random_partition.R
@@ -7,7 +7,8 @@ generate_random_partition <- function(
   cell_line_order,
   training_amount.c, 
   acc_training = NULL,
-  input_partition = NULL # if the test set is given
+  input_partition = NULL,
+  force_balance = TRUE # if the test set is given
   ) {
   
   stopifnot(!is.null(acc_training))
@@ -15,7 +16,7 @@ generate_random_partition <- function(
   stopifnot(length(labels_cell_lines) == length(cell_line_order))
   stopifnot(training_amount.p >= 10)
   stopifnot(length(labels_patient) > training_amount.p)
-  stopifnot(min(table(labels_patient)) >= 16)
+  stopifnot(min(table(labels_patient)) >= 8)
   
   require("doParallel")
 
@@ -62,17 +63,26 @@ generate_random_partition <- function(
       repeat {
         
         if (is.null(input_partition)) {
-          temp.training_index.p2p <- sample(temp.patient.false, 8)
-          temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patient.true, 8))
-          
-          temp.test_index <- sample(temp.patient.false[-which(temp.patient.false %in% temp.training_index.p2p)], 8)
-          temp.test_index <- c(temp.test_index, sample(temp.patient.true[-which(temp.patient.true %in% temp.training_index.p2p)], 8))
-          
-          temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patients[-c(temp.training_index.p2p, temp.test_index)], training_amount.p - 16))
-          stopifnot(min(table(labels_patient[temp.training_index.p2p])) >= 8)
+          if (force_balance) {
+            temp.training_index.p2p <- sample(temp.patient.false, 8)
+            temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patient.true, 8))
+            
+            temp.test_index <- sample(temp.patient.false[-which(temp.patient.false %in% temp.training_index.p2p)], 8)
+            temp.test_index <- c(temp.test_index, sample(temp.patient.true[-which(temp.patient.true %in% temp.training_index.p2p)], 8))
+            
+            temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patients[-c(temp.training_index.p2p, temp.test_index)], training_amount.p - 16))
+            stopifnot(min(table(labels_patient[temp.training_index.p2p])) >= 8)
+          } else {
+            temp.training_index.p2p <- sample(temp.patient.false, 5)
+            temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patient.true, 5))
+            temp.training_index.p2p <- c(temp.training_index.p2p, sample(temp.patients[-c(temp.training_index.p2p)], training_amount.p - 10))
+            stopifnot(min(table(labels_patient[temp.training_index.p2p])) >= 5)
+          }
           stopifnot(length(temp.training_index.p2p) == training_amount.p)
           temp.test_index <- setdiff(temp.patients, temp.training_index.p2p)
-          stopifnot(min(table(labels_patient[temp.test_index])) >= 8)
+          if (force_balance) {
+            stopifnot(min(table(labels_patient[temp.test_index])) >= 5)
+          }
         } else {
           temp.training_index.p2p <- input_partition$p2p[[temp.run_ind]]$training_index
           stopifnot(length(temp.training_index.p2p) == training_amount.p)
@@ -96,12 +106,12 @@ generate_random_partition <- function(
         }
         
         if (acc_training) {
-          if ((length(table(labels_patient[temp.training_index.p2p])) == 2 && min(table(labels_patient[temp.training_index.p2p])) >= 8)) {
+          if ((length(table(labels_patient[temp.training_index.p2p])) == 2 && min(table(labels_patient[temp.training_index.p2p])) >= 5)) {
             break
           }          
         } else {
-          if ((length(table(labels_patient[temp.test_index])) == 2 && min(table(labels_patient[temp.test_index])) >= 8
-               && length(table(labels_patient[temp.training_index.p2p])) == 2 && min(table(labels_patient[temp.training_index.p2p])) >= 8)) {
+          if ((length(table(labels_patient[temp.test_index])) == 2 && min(table(labels_patient[temp.test_index])) >= 5
+               && length(table(labels_patient[temp.training_index.p2p])) == 2 && min(table(labels_patient[temp.training_index.p2p])) >= 5)) {
             break
           }
         }
